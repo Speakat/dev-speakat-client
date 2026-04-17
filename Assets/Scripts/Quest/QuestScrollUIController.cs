c52dcbe0353fefabe2227ce2a102168c0e44c101
hye33 <128382222+hye33@users.noreply.github.com>
2026-04-17 18:18:20 +0900

    chore: questButton GetComponent 중복호출 제거 (#5)

--- Assets/Scripts/Quest/QuestScrollUIController.cs
+++ Assets/Scripts/Quest/QuestScrollUIController.cs
@@ -20,18 +20,20 @@ public class QuestScrollUIController : MonoBehaviour
         for (int i = 0; i < questCount; i++)
         {
             GameObject questButton = Instantiate(questButtonPrefab, transform);
+            QuestButtonPanel buttonPanel = questButton.GetComponent<QuestButtonPanel>();
 
             // 배치 예시(3가지 위치 반복)
             float dValue = 100f; // 위치 간격
             int d = i % 4;
-            if (d == 0)
+            if (d == 0) // 왼쪽
             {
-                questButton.GetComponent<QuestButtonPanel>().SetButtonPosition(-dValue);
+                buttonPanel.SetButtonPosition(-dValue);
             }
-            else if (d == 2)
+            else if (d == 2) // 오른쪽
             {
-                questButton.GetComponent<QuestButtonPanel>().SetButtonPosition(dValue);
+                buttonPanel.SetButtonPosition(dValue);
             }
+            // d == 1인 경우 중앙에 배치(위치 이동 x)
         }
     }
 
