f29f37cb99aaf4454f681e389d4f082b5cc32641
hye33 <128382222+hye33@users.noreply.github.com>
2026-05-30 06:08:30 +0900

    feat: 게임 플레이 세션 데이터 모델 클래스 정의 (#25)

---
+++ Assets/Scripts/GamePlay/GamePlayData.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+
+[System.Serializable]
+public class SessionResponse
+{
+    public bool isSuccess;
+    public SessionData data;
+}
+
+[System.Serializable]
+public class SessionData
+{
+    public string sessionId;
+    public string npcDialogue;
+}
+
+[System.Serializable]
+public class TurnResponse
+{
+    public bool isSuccess;
+    public TurnData data;
+}
+
+[System.Serializable]
+public class TurnData
+{
+    public string npcDialogue;
+    public string npcDialogueAudio;
+    public bool isTurnPassed;
+    public TurnEvaluation turnEvaluation;
+    public QuestResult questResult;
+}
+
+[System.Serializable]
+public class TurnEvaluation
+{
+    public float contextRelevance;
+    public float grammarAccuracy;
+    public float expressionQuality;
+    public List<string> objectiveProgress;
+    public bool isQuestComplete;
+    public string reason;
+    public List<string> betterSuggestions;
+    public string recommendationReason;
+}
+
+[System.Serializable]
+public class QuestResult
+{
+    public float averageContextRelevance;
+    public float averageGrammarAccuracy;
+    public float averageExpressionQuality;
+    public List<string> achievedObjectives;
+    public bool isQuestSuccess;
+}
