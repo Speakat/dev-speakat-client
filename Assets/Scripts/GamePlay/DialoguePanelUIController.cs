981a0e3a04e2f269447b8497d415b64b10eb0999
Hyerin <h5916847@gmail.com>
2026-06-01 06:48:41 +0900

    feat: Dialogue 패널 Clear 함수 추가 (#25)
    - Question과 Answer 패널을 비활성화시키는 ClearPanel 함수를 추가하였습니다.

--- Assets/Scripts/GamePlay/DialoguePanelUIController.cs
+++ Assets/Scripts/GamePlay/DialoguePanelUIController.cs
@@ -7,11 +7,19 @@ public class DialoguePanelUIController : MonoBehaviour
     private GameObject questionPanel;
     [SerializeField]
     private GameObject answerPanel;
+
+    [SerializeField] private TextMeshProUGUI questionText;
+    [SerializeField] private float maxWidth = 300f;
+    [SerializeField] private float horizontalPadding = 50f; // 좌우 패딩 합산
+
+    private RectTransform questionPanelRect;
     
     public void SetQuestionPanel(string question)
     {
         questionPanel.SetActive(true);
         questionPanel.GetComponentInChildren<TextMeshProUGUI>().text = question;
+
+        //Resize();
     }
 
     public void SetAnswerPanel(string answers)
@@ -19,4 +27,34 @@ public class DialoguePanelUIController : MonoBehaviour
         answerPanel.SetActive(true);
         answerPanel.GetComponentInChildren<TextMeshProUGUI>().text = answers;
     }
+
+    public void ClearPanels()
+    {
+        questionPanel.SetActive(false);
+        answerPanel.SetActive(false);
+    }
+
+    private void Awake()
+    {
+        ClearPanels();
+    }
+
+    // public void Resize()
+    // {
+    //     // 한 줄일 때 텍스트의 자연스러운 너비 계산
+    //     float preferredWidth = questionText.preferredWidth + horizontalPadding;
+
+    //     if (preferredWidth <= maxWidth)
+    //     {
+    //         // 텍스트가 짧으면 → 너비를 텍스트에 맞게 줄임, 한 줄
+    //         questionPanelRect.sizeDelta = new Vector2(preferredWidth, questionPanelRect.sizeDelta.y);
+    //         questionText.rectTransform.sizeDelta = new Vector2(preferredWidth - horizontalPadding, questionText.rectTransform.sizeDelta.y);
+    //     }
+    //     else
+    //     {
+    //         // 텍스트가 길면 → 최대 너비 고정, 여러 줄로 줄바꿈
+    //         questionPanelRect.sizeDelta = new Vector2(maxWidth, questionPanelRect.sizeDelta.y);
+    //         questionText.rectTransform.sizeDelta = new Vector2(maxWidth - horizontalPadding, questionText.rectTransform.sizeDelta.y);
+    //     }
+    // }
 }
